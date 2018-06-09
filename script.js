05ee167ab3bc5fc0b6e12b084161b82a12ccefe0
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-06-09 08:50:09 +0200

    menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+		$(".menuknap").on("click", function () {
+			$(".dropdown-content").toggleClass("expanded");
+			$('.nav-down').toggleClass("nav_bg");
+		});
 
+		$(".menulink").on("click", function () {
+			$(".dropdown-content").removeClass("expanded");
+			$(".nav-down").removeClass("nav_bg");
+		});
+		$(".nav-down").on("mouseleave", function () {
+			$(".dropdown-content").removeClass("expanded");
+			$(".nav-down").removeClass("nav_bg");
+		});
