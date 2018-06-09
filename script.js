b34a817a52599cd131718131515a9c7c55a22d60
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-06-09 10:20:32 +0200

    ny menu

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 		$(".menuknap").on("click", function () {
 			$(".dropdown-content").toggleClass("expanded");
-			$('.nav-down').toggleClass("nav_bg");
 		});
 
 		$(".menulink").on("click", function () {
 			$(".dropdown-content").removeClass("expanded");
-			$(".nav-down").removeClass("nav_bg");
 		});
 		$(".nav-down").on("mouseleave", function () {
 			$(".dropdown-content").removeClass("expanded");
-			$(".nav-down").removeClass("nav_bg");
 		});
