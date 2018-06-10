4776d3d7df096e650c0a44a83ae6adb45fe38390
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-06-10 23:18:18 +0200

    kanin hopper

--- script.js
+++ script.js
@@ -40,5 +40,30 @@ function kaninHopperInd() {
 	// Start lyd: effekt_kaninhop
 	$("#effekt_kaninhop")[0].play();
 	$("#effekt_kaninhop")[0].volume = 0.1;
+	document.getElementById("effekt_kaninhop").loop = true;
 
+	// når container er flyttet
+	$("#kanin_container").on("animationend", kaninStaar);
+}
+
+// - - - - - kanin_står- - - - -
+
+function kaninStaar() {
+	console.log("kaninStaar");
+
+	$("#kanin_container").off("animationend", kaninStaar);
+	// - - -
+	// Snorken skrues ned
+	$("#effekt_kaninhop")[0].pause();
+
+	// Start kanin move: kanin_ind-fra-siden
+	$("#kanin_container").removeClass("kanin_ind_fra_siden");
+	$("#kanin_container").addClass("kanin_inde");
+
+	// Start kanin-animation: kanin_hop_fremad
+	$("#kanin_sprite").removeClass("kanin_hop_fremad");
+	$("#kanin_sprite").removeClass("kanin_staar");
+
+	//
+	//	$("#kanin_container").on("animationend", kaninStaar);
 }
